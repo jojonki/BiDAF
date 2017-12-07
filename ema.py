6e808bea8d2caf1d6df81cc58ae29a7fe19a8fa4
jojonki <junki.ohmura@gmail.com>
2017-12-07 11:30:35 -0500

    add EMA class

---
+++ ema.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+class EMA(nn.Module):
+    def __init__(self, mu):
+        super(EMA, self).__init__()
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, name, val):
+        self.shadow[name] = val.clone()
+
+    def forward(self, name, x):
+        assert name in self.shadow
+        new_average = self.mu * x + (1.0 - self.mu) * self.shadow[name]
+        self.shadow[name] = new_average.clone()
+        return new_average
